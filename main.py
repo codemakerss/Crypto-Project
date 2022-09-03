ab63fe92a216f404d21166b62d351b8250882ec5
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-09-03 14:08:47 -0400

    finish function test

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ apikey = "BF4TLBIGC0D0F8RY"
 market_currency = "CNY"
 
 CrytoAPI = CrytoAPI(apikey,market_currency)
-result = CrytoAPI.Get_Crypto_Daily_Price("BTC")
+result = CrytoAPI.Get_Crypto_Daily_Weekly_Monthly_Price("BTC","monthly")
 print(result)
