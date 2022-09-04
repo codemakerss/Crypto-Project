102e0f498f2686806e00fe0c8ba0a5d3844090b6
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-09-04 12:22:06 -0400

    test interval function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from CryptoAPIFile.crypto_api import *
 apikey = "BF4TLBIGC0D0F8RY"
 market_currency = "CNY"
 
-CrytoAPI = CrytoAPI(apikey,market_currency)
-result = CrytoAPI.Get_Crypto_Daily_Weekly_Monthly_Price("BTC","monthly")
+CryptoAPI = CryptoAPI(apikey,market_currency)
+#result = CryptoAPI.Get_Crypto_Daily_Weekly_Monthly_Price("BTC","monthly")
+result = CryptoAPI.Get_Crypto_Interval_Price("BTC","5min")
 print(result)
