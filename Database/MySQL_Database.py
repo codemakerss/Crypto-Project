a170c42701ee1f36eefa984d10147b67b921e3be
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-09-04 13:20:21 -0400

    update functions

--- Database/MySQL_Database.py
+++ Database/MySQL_Database.py
@@ -35,23 +35,32 @@ class Data_to_SQL(object):
 
     # define table datatypes 
     def table_datatypes(self, table_name : str):
-        if table_name == 'daily':
-            table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), volume int(11))'
-        elif table_name == 'weekly':
-            table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), adjusted_close DECIMAL(10,5), volume int(11), dividend_amt DECIMAL(10,5))'
-        elif table_name == 'monthly':
-            table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), adjusted_close DECIMAL(10,5), volume int(11), dividend_amt DECIMAL(10,5))'
-        elif table_name == 'intraday':
-            table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATETIME, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), volume INT(11))'
-        elif table_name == 'company_info':
-            table_datatype = '(Symbol VARCHAR(225) NOT NULL, AssetType VARCHAR(225), Name VARCHAR(225), Exchange VARCHAR(225), Country VARCHAR(225), Sector VARCHAR(225), Industry VARCHAR(225), IpoDate DATE, DelistingDate DATE NULL, Status VARCHAR(225), PRIMARY KEY (symbol))'   
-        elif table_name == 'search':
-            table_datatype = '(Symbol VARCHAR(225) NOT NULL PRIMARY KEY, Name VARCHAR(225), Type VARCHAR(225), Region VARCHAR(225), MarketOpen TIME, MarketClose TIME, Timezone VARCHAR(225), Currency VARCHAR(225), MatchScore DECIMAL(7,5))' 
-        elif table_name == 'listinganddelisting':
-            table_datatype = '(symbol VARCHAR(225) NOT NULL, name VARCHAR(225), exchange VARCHAR(225), assetType VARCHAR(225), ipoDate DATE, delistingDate DATE NULL DEFAULT NULL, status VARCHAR(225), PRIMARY KEY (symbol))'  
-        elif table_name == 'IPOCalender':
-            table_datatype = '(symbol VARCHAR(225) NOT NULL PRIMARY KEY, name VARCHAR(225), ipoDate DATE, priceRangeLow DECIMAL(10,4), priceRangeHigh DECIMAL(10,4), currency VARCHAR(225), exchange VARCHAR(225))'
-        else:
-            print('No data support! ')
+        try:
+            if table_name == 'daily':
+                table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), volume int(11))'
+            elif table_name == 'weekly':
+                table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), adjusted_close DECIMAL(10,5), volume int(11), dividend_amt DECIMAL(10,5))'
+            elif table_name == 'monthly':
+                table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATE, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), adjusted_close DECIMAL(10,5), volume int(11), dividend_amt DECIMAL(10,5))'
+            elif table_name == 'intraday':
+                table_datatype = '(id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, symbol VARCHAR(255) NOT NULL, datetime DATETIME, open DECIMAL(10,5), high DECIMAL(10,5), low DECIMAL(10,5), close DECIMAL(10,5), volume INT(11))'
+            elif table_name == 'Information':
+                table_datatype = '(Symbol VARCHAR(225) NOT NULL, AssetType VARCHAR(225), Name VARCHAR(225), Exchange VARCHAR(225), Country VARCHAR(225), Sector VARCHAR(225), Industry VARCHAR(225), IpoDate DATE, DelistingDate DATE NULL, Status VARCHAR(225), PRIMARY KEY (symbol))'   
+            elif table_name == 'Digital Currency Code':
+                table_datatype = '(Symbol VARCHAR(225) NOT NULL PRIMARY KEY, Name VARCHAR(225), Type VARCHAR(225), Region VARCHAR(225), MarketOpen TIME, MarketClose TIME, Timezone VARCHAR(225), Currency VARCHAR(225), MatchScore DECIMAL(7,5))' 
+            elif table_name == 'Digital Currency Name':
+                table_datatype = '(symbol VARCHAR(225) NOT NULL, name VARCHAR(225), exchange VARCHAR(225), assetType VARCHAR(225), ipoDate DATE, delistingDate DATE NULL DEFAULT NULL, status VARCHAR(225), PRIMARY KEY (symbol))'  
+            elif table_name == 'Market Code':
+                table_datatype = '(symbol VARCHAR(225) NOT NULL PRIMARY KEY, name VARCHAR(225), ipoDate DATE, priceRangeLow DECIMAL(10,4), priceRangeHigh DECIMAL(10,4), currency VARCHAR(225), exchange VARCHAR(225))'
+            elif table_name == 'Market Name':
+                table_datatype = ''
+            elif table_name == 'Time Zone':
+                table_datatype = ''    
+            # else:
+            #     print('No data support! ')
+            return table_datatype
+        except:
+            raise Exception("No datatype support !")
         
-        return table_datatype
+        
+        
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from CryptoAPIFile.crypto_api import *
 
 apikey = "BF4TLBIGC0D0F8RY"
-market_currency = "CNY"
+market_currency = "USD"
 
 CryptoAPI = CryptoAPI(apikey,market_currency)
 #result = CryptoAPI.Get_Crypto_Daily_Weekly_Monthly_Price("BTC","monthly")
 result = CryptoAPI.Get_Crypto_Interval_Price("BTC","5min")
-print(result)
+print(result[0])
