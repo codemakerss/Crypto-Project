671e57d872873101b41285d94395fd2f9836614a
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-09-07 22:06:05 -0400

    test mysql connection error

--- Main/main.py
+++ Main/main.py
@@ -8,14 +8,15 @@ market_currency = "CNY"
 # crypto data test
 CryptoAPI = CryptoAPI(apikey,market_currency)
 #result = CryptoAPI.Get_Crypto_Daily_Weekly_Monthly_Price("BTC","monthly")
-result = CryptoAPI.Get_Crypto_Interval_Price("BTC","5min")
-print(result)
+#result = CryptoAPI.Get_Crypto_Interval_Price("BTC","5min")
+#print(result)
 
 # mysql connection test 
-# host="localhost" 
-# user="root"
-# password="Dhy9904191asd@@" 
-# database="crytodatatest"
+host="localhost" 
+user="root"
+password="Dhy9904191asd@@" 
+database="crytodatatest"
+print(mysql.connector.connect(host = host, user = user, password = password, database = database))
 # MySQL_Database = MySQL_Database(host = host, user = user, password = password, database = database)
 # connection_test = MySQL_Database.connect_database()
 # print(connection_test)
