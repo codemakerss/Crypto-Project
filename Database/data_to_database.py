1c62ac9f2531b1da625c961137151b65c6a506ad
Jeff <51349563+codemakerss@users.noreply.github.com>
2022-09-11 16:36:55 -0400

    fix issuse

--- Database/data_to_database.py
+++ Database/data_to_database.py
@@ -1,5 +1,28 @@
+#from construct_database import *
 
 class Data_to_SQL(object):
-    # Call mysql connection and API here 
+    """
+    This class is for inserting data to mysql database
+
+    Attributes 
+    ----------
+    mysql_database : classmethod
+
+    Methods
+    ----------
+    connect_database(self)
+        connect to mysql database and create database if not exists 
+    
+    table_datatypes(self, table_name : str)
+        create database table datatypes
+
+    create_table(self, table_name : str)
+        create tables by selecting the correct datatypes
+    """
     def __init__(self, mysql_database : classmethod) -> None:
         self.mysql_connection = mysql_database.connect_database()
+
+    def insert_mysql_data(self, table):
+        pass
+    
+    
